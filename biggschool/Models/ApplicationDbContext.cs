cf02299eb9c45ec1708a2f61ae444968f7cfd05e
Nguyen An <nguyenngocanit002@gmail.com>
2023-03-05 16:25:53 +0700

    Add project files.

---
+++ biggschool/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace biggschool.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext()
+            : base("biggschoolConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
